4d02c309f174aaba5442f5b70db737c03426a71b
pd0687 <42360703+pd0687@users.noreply.github.com>
2018-10-05 19:09:01 -0400

    Update R_capstone.R

--- R_capstone.R
+++ R_capstone.R
@@ -2,6 +2,7 @@ levels <- read.csv("levels2.csv")
 # levels_Canada <- as.ts(levels[,3])
 levels_Canada <- ts(levels[,3], frequency = 4)
 total_res <- 0
+total_AIC <- 0 # initialize AIC
 count <- 0
 len <- as.numeric(length(levels_Canada))-8
 
@@ -19,8 +20,15 @@ for (i in 1:len) {
   
   res_loop <- as.numeric(Canada_train[length(Canada_train)] - predict(AR_Canada)$pred)
   total_res <- total_res + res_loop^2 # square the residual
-  count <- count + 1
+
+  model_AIC <- AR_Canada$aic # compute AIC
+  total_AIC <- total_AIC + model_AIC # aggregate AIC
+  
   print(total_res)
+  print(model_AIC) # print AIC
+
+  count <- count + 1
 }
 
+AIC_avg <- total_AIC / count # average the AIC
 RMSE <- sqrt(total_res) / count
